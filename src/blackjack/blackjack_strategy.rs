ae3b7a8c7b6db93bfcbcfd168a9ae874248d6c3d
Michael Pe <mpohl100@googlemail.com>
2023-02-04 20:58:22 +0100

    added BlackjackStrategyVec

--- src/blackjack/blackjack_strategy.rs
+++ src/blackjack/blackjack_strategy.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::collections::BTreeMap;
 use crate::blackjack::blackjack_situation::HandSituation;
 use crate::blackjack::blackjack_situation::SplitSituation;
@@ -242,5 +243,141 @@ impl BlackjackStrategyTrait for CountedBlackjackStrategy{
             ret.push_str(&("Strategy: ".to_owned() + &strat.to_string_mat2() + &"\n\n".to_string()));
         }
         ret
+    }
+}
+
+
+#[derive(Default, Clone, Copy)]
+struct SituationStrategy<T>{
+    pub situation: T,
+    pub do_it: bool,
+}
+
+type HandSituationStrategy = SituationStrategy<HandSituation>;
+type SplitSituationStrategy = SituationStrategy<SplitSituation>;
+
+#[derive(Default, Clone)]
+struct BlackjackStrategyDataVec{
+    pub drawing_percentages: Vec<HandSituationStrategy>,
+    pub double_down_percentages: Vec<HandSituationStrategy>,
+    pub split_percentages: Vec<SplitSituationStrategy>,
+}
+
+#[derive(Default, Clone)]
+struct BlackjackStrategyVec{
+    data: BlackjackStrategyDataVec,
+    reversed: bool,
+}
+
+impl BlackjackStrategyVec{
+    pub fn new(reversed: bool) -> BlackjackStrategyVec{
+        BlackjackStrategyVec { data: BlackjackStrategyDataVec::default(), reversed: reversed }
+    }
+}
+
+impl BlackjackStrategyTrait for BlackjackStrategyVec{
+    fn get_draw(&self, situation: HandSituation, _deck: &Box<dyn Deck>) -> bool
+    {
+        if !self.reversed {
+            let mut iter = self.data.drawing_percentages.iter();
+            let res = iter.find(|x| x.situation == situation);
+            match res{
+                Some(value) => { value.do_it },
+                _ => panic!("Couldn't find hand situation in drawing percentages"),
+            }    
+        }
+        else {
+            let mut iter = self.data.drawing_percentages.iter().rev();
+            let res = iter.find(|x| x.situation == situation);
+            match res{
+                Some(value) => { value.do_it },
+                _ => panic!("Couldn't find hand situation in drawing percentages"),
+            }    
+        }
+    }
+
+    fn get_double_down(&self, situation: HandSituation, _deck: &Box<dyn Deck>) -> bool
+    {
+        if !self.reversed{
+            let mut iter = self.data.double_down_percentages.iter();
+            let res = iter.find(|x| x.situation == situation);
+            match res{
+                Some(value) => { value.do_it },
+                _ => panic!("Couldn't find hand situation in double down percentages"),
+            }    
+        }
+        else{
+            let mut iter = self.data.double_down_percentages.iter().rev();
+            let res = iter.find(|x| x.situation == situation);
+            match res{
+                Some(value) => { value.do_it },
+                _ => panic!("Couldn't find hand situation in double down percentages"),
+            }
+        }
+    }
+
+    fn get_split(&self, situation: SplitSituation, _deck: &Box<dyn Deck>) -> bool
+    {
+        if !self.reversed{   
+            let mut iter = self.data.split_percentages.iter();
+            let res = iter.find(|x| x.situation == situation);
+            match res{
+                Some(value) => { value.do_it },
+                _ => panic!("Couldn't find split situation in split percentages"),
+            }
+        }
+        else {
+            let mut iter = self.data.split_percentages.iter().rev();
+            let res = iter.find(|x| x.situation == situation);
+            match res{
+                Some(value) => { value.do_it },
+                _ => panic!("Couldn't find split situation in split percentages"),
+            }
+        }
+    }
+
+    fn add_draw(&mut self, situation: HandSituation, do_it: bool)
+    {
+        let mut iter = self.data.drawing_percentages.iter_mut();
+        let res = iter.find(|x| x.situation == situation);
+        match res{
+            Some(value) => { value.do_it = do_it; },
+            _ => { self.data.drawing_percentages.push(HandSituationStrategy{situation: situation, do_it: do_it})},
+        }
+    }
+
+    fn add_double_down(&mut self, situation: HandSituation, do_it: bool)
+    {
+        let mut iter = self.data.double_down_percentages.iter_mut();
+        let res = iter.find(|x| x.situation == situation);
+        match res{
+            Some(value) => { value.do_it = do_it; },
+            _ => { self.data.double_down_percentages.push(HandSituationStrategy{situation: situation, do_it: do_it})},
+        }
+    }
+
+    fn add_split(&mut self, situation: SplitSituation, do_it: bool)
+    {
+        let mut iter = self.data.split_percentages.iter_mut();
+        let res = iter.find(|x| x.situation == situation);
+        match res{
+            Some(value) => { value.do_it = do_it; },
+            _ => { self.data.split_percentages.push(SplitSituationStrategy{situation: situation, do_it: do_it})},
+        }
+    }
+
+    fn to_string_mat2(&self) -> String
+    {
+        let mut blackjack_strategy_sorted = BlackjackStrategy::new();
+        for situation_strategy in &self.data.drawing_percentages{
+            blackjack_strategy_sorted.add_draw(situation_strategy.situation, situation_strategy.do_it);
+        }
+        for situation_strategy in &self.data.double_down_percentages{
+            blackjack_strategy_sorted.add_double_down(situation_strategy.situation, situation_strategy.do_it);
+        }
+        for situation_strategy in &self.data.split_percentages{
+            blackjack_strategy_sorted.add_split(situation_strategy.situation, situation_strategy.do_it);
+        }
+        blackjack_strategy_sorted.to_string_mat2()
     }
 }
