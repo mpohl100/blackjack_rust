f9d4baee590fb5329f30c66a3cbe19d9761880be
Michael Pe <mpohl100@googlemail.com>
2023-05-27 17:11:19 +0200

    added performance test

---
+++ src/play_performance.rs
@@ -0,0 +1,29 @@
+
+mod blackjack;
+
+use crate::blackjack::traits::BlackjackStrategyTrait;
+use crate::blackjack::blackjack_strategy::BlackjackStrategy;
+use crate::blackjack::blackjack_strategy::BlackjackStrategyVec;
+use std::time::Instant;
+
+
+fn play<BlackjackStrategyType>(blackjack_strategy: BlackjackStrategyType, n: i32, description: String)
+where BlackjackStrategyType: BlackjackStrategyTrait + Clone
+{
+    let start = Instant::now();
+    let strat = blackjack::blackjack_analysis::optimize_blackjack(blackjack_strategy, 0);
+    let result = blackjack::play_blackjack::play_blackjack(n, &strat, false);
+    let duration = start.elapsed();
+    // Print the elapsed time
+    println!("{:?} time: {:?}", description, duration);
+    println!("result: {} after {} hands", result, n);
+}
+
+fn main() {
+    println!("Measure performance:");
+    let n = 1000000;
+    play(BlackjackStrategy::new(true), n, "HashMap".to_string());
+    play(BlackjackStrategy::new(false), n, "OrderedMap".to_string());
+    play(BlackjackStrategyVec::new(false), n, "ReversedVec".to_string());
+    play(BlackjackStrategyVec::new(true), n, "Vec".to_string());
+}
