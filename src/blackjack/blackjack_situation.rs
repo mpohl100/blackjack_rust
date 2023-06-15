c672e6992310442feb5ea4bcebe86335ebaeca99
Michael Pe <mpohl100@googlemail.com>
2023-06-15 17:30:43 +0200

    added tests for blackjack_situation.rs

--- src/blackjack/blackjack_situation.rs
+++ src/blackjack/blackjack_situation.rs
@@ -1,9 +1,10 @@
 use crate::blackjack::blackjack_points::Points;
 use crate::blackjack::card::BlackjackRank;
+use crate::blackjack::card::Rank;
 use crate::blackjack::traits::Allable;
 use crate::blackjack::traits::Stringable;
 
-#[derive(Copy, Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Debug, Copy, Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct BlackjackSituation<T> {
     situation: T,
     dealer_card: BlackjackRank,
@@ -45,3 +46,124 @@ impl<T: Allable + Clone> BlackjackSituation<T> {
 
 pub type HandSituation = BlackjackSituation<Points>;
 pub type SplitSituation = BlackjackSituation<BlackjackRank>;
+
+#[cfg(test)]
+mod hand_situation_tests {
+    use super::*;
+    
+    #[test]
+    fn test_situation() {
+        let situation = Points::new(10, 20);
+        let dealer_card = BlackjackRank::new(Rank::Deuce);
+        let blackjack_situation = HandSituation::new(situation, dealer_card);
+        
+        assert_eq!(blackjack_situation.situation(), situation);
+    }
+    
+    #[test]
+    fn test_dealer_card() {
+        let situation = Points::new(10, 20);
+        let dealer_card = BlackjackRank::new(Rank::Deuce);
+        let blackjack_situation = HandSituation::new(situation, dealer_card);
+        
+        assert_eq!(blackjack_situation.dealer_card(), dealer_card);
+    }
+    
+    #[test]
+    fn test_to_string_internal() {
+        let situation = Points::new(10, 20);
+        let dealer_card = BlackjackRank::new(Rank::Deuce);
+        let blackjack_situation = HandSituation::new(situation, dealer_card);
+        
+        let expected_string = "10/20|2".to_string();
+        assert_eq!(blackjack_situation.to_string_internal(), expected_string);
+    }
+    
+    #[test]
+    fn test_new() {
+        let situation = Points::new(10, 20);
+        let dealer_card = BlackjackRank::new(Rank::Deuce);
+        let blackjack_situation = HandSituation::new(situation, dealer_card);
+        
+        assert_eq!(blackjack_situation.situation(), situation);
+        assert_eq!(blackjack_situation.dealer_card(), dealer_card);
+    }
+    
+    #[test]
+    fn test_create_all() {
+        let all_situations = HandSituation::create_all();
+        
+        // Add assertions to check the expected number of situations
+        let expected_len = 30 * 10;
+        assert_eq!(all_situations.len(), expected_len);
+        
+        let mut index = 0;
+        for sit in Points::create_all(){
+            for dc in BlackjackRank::create_all(){
+                assert_eq!(all_situations[index], HandSituation::new(sit, dc));
+                index += 1;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod split_situation_tests {
+    use super::*;
+    
+    #[test]
+    fn test_situation() {
+        let situation = BlackjackRank::new(Rank::Ten);
+        let dealer_card = BlackjackRank::new(Rank::Deuce);
+        let blackjack_situation = SplitSituation::new(situation, dealer_card);
+        
+        assert_eq!(blackjack_situation.situation(), situation);
+    }
+    
+    #[test]
+    fn test_dealer_card() {
+        let situation = BlackjackRank::new(Rank::Ten);
+        let dealer_card = BlackjackRank::new(Rank::Deuce);
+        let blackjack_situation = SplitSituation::new(situation, dealer_card);
+        
+        assert_eq!(blackjack_situation.dealer_card(), dealer_card);
+    }
+    
+    #[test]
+    fn test_to_string_internal() {
+        let situation = BlackjackRank::new(Rank::Ten);
+        let dealer_card = BlackjackRank::new(Rank::Deuce);
+        let blackjack_situation = SplitSituation::new(situation, dealer_card);
+        
+        let expected_string = "10|2".to_string();
+        assert_eq!(blackjack_situation.to_string_internal(), expected_string);
+    }
+    
+    #[test]
+    fn test_new() {
+        let situation = BlackjackRank::new(Rank::Ten);
+        let dealer_card = BlackjackRank::new(Rank::Deuce);
+        let blackjack_situation = SplitSituation::new(situation, dealer_card);
+        
+        assert_eq!(blackjack_situation.situation(), situation);
+        assert_eq!(blackjack_situation.dealer_card(), dealer_card);
+    }
+    
+    #[test]
+    fn test_create_all() {
+        let all_situations = SplitSituation::create_all();
+        
+        // Add assertions to check the expected number of situations
+        let expected_len = 10 * 10;
+        assert_eq!(all_situations.len(), expected_len);
+        
+        let mut index = 0;
+        for sit in BlackjackRank::create_all(){
+            for dc in BlackjackRank::create_all(){
+                assert_eq!(all_situations[index], SplitSituation::new(sit, dc));
+                index += 1;
+            }
+        }
+    }
+}
+
