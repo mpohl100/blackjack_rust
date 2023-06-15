b549d058dba96b252e55a1913f66c5c48c962bfd
Michael Pe <mpohl100@googlemail.com>
2023-06-15 08:05:08 +0200

    added tests in blackjack_points. deleted unnecessary file

--- src/blackjack/blackjack_points.rs
+++ src/blackjack/blackjack_points.rs
@@ -1,7 +1,7 @@
 use super::traits::Allable;
 use super::traits::Stringable;
 
-#[derive(Default, Copy, Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Debug, Default, Copy, Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Points {
     lower: i32,
     upper: i32,
@@ -38,5 +38,50 @@ impl Allable for Points{
             points.insert(Points::new(i, i + 10 ));
         }
         points.into_iter().collect()
+    }
+}
+
+#[cfg(test)]
+mod points_tests {
+    use super::*;
+
+    #[test]
+    fn test_new_with_upper_above_21() {
+        let points = Points::new(15, 25);
+
+        assert_eq!(points.lower(), 15);
+        assert_eq!(points.upper(), 15);
+    }
+
+    #[test]
+    fn test_new_with_upper_below_21() {
+        let points = Points::new(8, 18);
+
+        assert_eq!(points.lower(), 8);
+        assert_eq!(points.upper(), 18);
+    }
+
+    #[test]
+    fn test_to_string_internal() {
+        let points = Points::new(10, 20);
+
+        assert_eq!(points.to_string_internal(), "10/20".to_string());
+    }
+
+    #[test]
+    fn test_create_all() {
+        let all_points = Points::create_all();
+        let expected_length = 19 + 11; // range [2-21] + 11 points with 10 added
+
+        assert_eq!(all_points.len(), expected_length);
+
+        let mut expected_points = std::collections::BTreeSet::new();
+        for i in 2..=21 {
+            expected_points.insert(Points::new(i, i));
+            expected_points.insert(Points::new(i, i + 10));
+        }
+        let expected_points_vec: Vec<Points> = expected_points.into_iter().collect();
+        println!("Len comp.: {:?} {:?}", all_points.len(), expected_points_vec.len());
+        assert_eq!(all_points, expected_points_vec);
     }
 }
