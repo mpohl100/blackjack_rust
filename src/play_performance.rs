fea34ae0ba59c9d3db06f60217c76e713db8a9b5
Michael Pohl <mpohl100@googlemail.com>
2024-04-16 19:06:08 +0000

    cargo fmt

--- src/play_performance.rs
+++ src/play_performance.rs
@@ -3,9 +3,9 @@ mod commandline_params;
 
 use crate::blackjack::analysis::blackjack_analysis::optimize_blackjack;
 use crate::blackjack::play_blackjack::play_blackjack;
-use crate::blackjack::strategy::blackjack_strategy_combined_vec::BlackjackStrategyCombinedVec;
 use crate::blackjack::strategy::blackjack_strategy_combined_hash_map::BlackjackStrategyCombinedHashMap;
 use crate::blackjack::strategy::blackjack_strategy_combined_ordered_hash_map::BlackjackStrategyCombinedOrderedHashMap;
+use crate::blackjack::strategy::blackjack_strategy_combined_vec::BlackjackStrategyCombinedVec;
 use crate::blackjack::strategy::blackjack_strategy_map::BlackjackStrategy;
 use crate::blackjack::strategy::blackjack_strategy_vec::BlackjackStrategyVec;
 use crate::blackjack::traits::BlackjackStrategyTrait;
