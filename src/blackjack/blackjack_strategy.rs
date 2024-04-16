1c9f37623c3c613d0ee89bd21f39f5b521ee36c1
Michael Pohl <mpohl100@googlemail.com>
2024-04-16 14:59:55 +0000

    added blackjack strategy combined hash map

--- src/blackjack/blackjack_strategy.rs
+++ src/blackjack/blackjack_strategy.rs
@@ -3,6 +3,7 @@ use std::collections::BTreeMap;
 use std::collections::HashMap;
 use crate::blackjack::blackjack_situation::HandSituation;
 use crate::blackjack::blackjack_situation::SplitSituation;
+use crate::blackjack::blackjack_situation::GameSituation;
 use crate::blackjack::blackjack_points::Points;
 use crate::blackjack::card::BlackjackRank;
 use crate::blackjack::traits::Allable;
@@ -453,6 +454,91 @@ impl BlackjackStrategyTrait for BlackjackStrategyVec{
 
         for split_situation in &self.data.split_percentages{
             result.split_percentages.insert(split_situation.situation, split_situation.do_it);
+        }
+        result
+    }
+}
+
+pub struct BlackjackStrategyCombinedHashMap{
+    data: HashMap<GameSituation, bool>,
+}
+
+impl BlackjackStrategyCombinedHashMap{
+    pub fn new() -> BlackjackStrategyCombinedHashMap{
+        BlackjackStrategyCombinedHashMap{data: HashMap::new()}
+    }
+}
+
+impl BlackjackStrategyTrait for BlackjackStrategyCombinedHashMap{
+    
+    fn add_draw(&mut self, situation: HandSituation, do_it: bool) {
+        self.data.insert(GameSituation::Draw(situation), do_it);
+    }
+
+    fn add_double_down(&mut self, situation: HandSituation, do_it: bool) {
+        self.data.insert(GameSituation::DoubleDown(situation), do_it);
+    }
+
+    fn add_split(&mut self, situation: SplitSituation, do_it: bool) {
+        self.data.insert(GameSituation::Split(situation), do_it);
+    }
+
+    fn get_draw(&self, situation: HandSituation, _deck: &Box<dyn Deck>) -> bool {
+        match self.data.get(&GameSituation::Draw(situation)){
+            Some(value) => {*value},
+            _ => panic!("Couldn't find draw hand situation in drawing percentages"),
+        }
+    }
+
+    fn get_double_down(&self, situation: HandSituation, deck: &Box<dyn Deck>) -> bool {
+        match self.data.get(&GameSituation::DoubleDown(situation)){
+            Some(value) => {*value},
+            _ => panic!("Couldn't find double down situation in double down percentages"),
+        }
+    }
+
+    fn get_split(&self, situation: SplitSituation, deck: &Box<dyn Deck>) -> bool {
+        match self.data.get(&GameSituation::Split(situation)){
+            Some(value) => {*value},
+            _ => panic!("Couldn't find split situation in split percentages"),
+        }
+    }
+
+    fn to_string_mat2(&self) -> String {
+        let mut blackjack_strategy = BlackjackStrategy::new(false);
+        for (situation, do_it) in &self.data{
+            match situation{
+                GameSituation::Draw(hand_situation) => {blackjack_strategy.add_draw(*hand_situation, *do_it);},
+                GameSituation::DoubleDown(hand_situation) => {blackjack_strategy.add_double_down(*hand_situation, *do_it);},
+                GameSituation::Split(split_situation) => {blackjack_strategy.add_split(*split_situation, *do_it);},
+            }
+        }
+        blackjack_strategy.to_string_mat2()
+    }
+
+    fn combine(&mut self, blackjack_strategy: &BlackjackStrategyData) {
+        for (sit, do_it) in blackjack_strategy.drawing_percentages.iter(){
+            self.add_draw(*sit, *do_it);
+        }
+        
+        for (sit, do_it) in blackjack_strategy.double_down_percentages.iter(){
+            self.add_double_down(*sit, *do_it);
+        }
+
+        for (sit, do_it) in blackjack_strategy.split_percentages.iter(){
+            self.add_split(*sit, *do_it);
+        }
+    }
+
+    fn dump(&self) -> BlackjackStrategyData {
+        let mut result = BlackjackStrategyData::default();
+
+        for (situation, do_it) in &self.data{
+            match situation{
+                GameSituation::Draw(hand_situation) => {result.drawing_percentages.insert(*hand_situation, *do_it);},
+                GameSituation::DoubleDown(hand_situation) => {result.double_down_percentages.insert(*hand_situation, *do_it);},
+                GameSituation::Split(split_situation) => {result.split_percentages.insert(*split_situation, *do_it);},
+            }
         }
         result
     }
