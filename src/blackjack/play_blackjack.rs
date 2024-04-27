5d872631520727098e7ff8dc81709bb6454479b1
Michael Pohl <mpohl100@googlemail.com>
2024-04-27 17:11:09 +0000

    finished introducing WrappedDeck

--- src/blackjack/play_blackjack.rs
+++ src/blackjack/play_blackjack.rs
@@ -9,25 +9,27 @@ use crate::blackjack::play_blackjack_hand::PlayMode;
 use crate::blackjack::rng::RandomNumberGenerator;
 use crate::blackjack::traits::BlackjackStrategyTrait;
 
+use super::deck::WrappedDeck;
+
 pub async fn play_blackjack(
     play_config: PlayConfiguration,
     blackjack_strategy: &mut dyn BlackjackStrategyTrait,
 ) -> f64 {
-    let mut boxed_deck: Box<dyn Deck + Send> = match play_config.play_normal {
-        true => Box::new(EightDecks::new()),
-        false => Box::new(CountedDeck::new(0)),
+    let mut boxed_deck: WrappedDeck = match play_config.play_normal {
+        true => WrappedDeck::new(Box::new(EightDecks::new())),
+        false => WrappedDeck::new(Box::new(CountedDeck::new(0))),
     };
     let mut rng = RandomNumberGenerator::new();
     let mut result = 0.0;
     let blackjack_game = blackjack_strategy.upcast_mut();
     for _ in 0..play_config.nb_hands {
         let player_hand = PlayerHand::new(&[
-            boxed_deck.deal_card(&mut rng),
-            boxed_deck.deal_card(&mut rng),
+            boxed_deck.get().deal_card(&mut rng),
+            boxed_deck.get().deal_card(&mut rng),
         ]);
         let dealer_hand = DealerHand::new(&[
-            boxed_deck.deal_card(&mut rng),
-            boxed_deck.deal_card(&mut rng),
+            boxed_deck.get().deal_card(&mut rng),
+            boxed_deck.get().deal_card(&mut rng),
         ]);
         result += play_blackjack_hand(
             1.0,
