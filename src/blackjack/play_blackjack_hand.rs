4b7a7b86ad7539a0b6e57d76f9182f32ae0b0e2d
Michael Pohl <mpohl100@googlemail.com>
2024-04-27 18:54:18 +0000

    made async play_blackjack_hand compile

--- src/blackjack/play_blackjack_hand.rs
+++ src/blackjack/play_blackjack_hand.rs
@@ -50,19 +50,19 @@ pub async fn play_blackjack_hand(
     play_mode: PlayMode,
 ) -> f64 {
     // play dealer hand at the beginning so that recursive versions for splitting use the same dealer outcome
-    let dealer_result = dealer_hand.play(&mut deck, rng);
+    let dealer_result = dealer_hand.play(deck, rng);
 
     // add code for splitting here
     if play_mode == PlayMode::All && player_hand.is_pair() {
         // splitting hands is allowed
         let rank = BlackjackRank::new(player_hand.get_cards()[0].rank());
         let do_split =
-            player_strategy.get_split(SplitSituation::new(rank, dealer_hand.open_card()), &mut deck);
+            player_strategy.get_split(SplitSituation::new(rank, dealer_hand.open_card()), deck);
         if do_split.await {
             let first = PlayerHand::new(&[player_hand.get_cards()[0], deck.get().deal_card(rng)]);
             let second = PlayerHand::new(&[player_hand.get_cards()[1], deck.get().deal_card(rng)]);
             let mut overall_result = 0.0;
-            overall_result += play_blackjack_hand(
+            overall_result += Box::pin(play_blackjack_hand(
                 player_bet,
                 first,
                 dealer_hand.clone(),
@@ -70,8 +70,8 @@ pub async fn play_blackjack_hand(
                 player_strategy,
                 rng,
                 play_mode,
-            ).await;
-            overall_result += play_blackjack_hand(
+            )).await;
+            overall_result += Box::pin(play_blackjack_hand(
                 player_bet,
                 second,
                 dealer_hand.clone(),
@@ -79,7 +79,7 @@ pub async fn play_blackjack_hand(
                 player_strategy,
                 rng,
                 play_mode,
-            ).await;
+            )).await;
             return overall_result;
         }
     }
@@ -90,7 +90,7 @@ pub async fn play_blackjack_hand(
         player_points = evaluate_blackjack_hand(&player_hand.get_blackjack_hand());
         only_draw_once = player_strategy.get_double_down(
             HandSituation::new(player_points, dealer_hand.open_card()),
-            &mut deck,
+            deck,
         ).await;
         if only_draw_once {
             player_bet *= 2.0;
@@ -108,7 +108,7 @@ pub async fn play_blackjack_hand(
             }
             let draw = player_strategy.get_draw(
                 HandSituation::new(player_points, dealer_hand.open_card()),
-                &mut deck,
+                deck,
             ).await;
             if !draw {
                 break;
