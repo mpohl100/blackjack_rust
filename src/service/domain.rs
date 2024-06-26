b5fcd921157fdb0c6b461b835d7915e3f111da02
Michael Pohl <mpohl100@googlemail.com>
2024-06-26 22:44:56 +0000

    got domain to build

--- src/service/domain.rs
+++ src/service/domain.rs
@@ -1,29 +1,33 @@
-use blackjack::game::channel_game::ChannelGame;
-use std::collections::HashMap;
-use uuid::Uuid;
-use tokio::sync::{mpsc, Mutex};
+use crate::game::channel_game::ChannelGame;
+use crate::game::channel_game::GameAction;
 
-pub struct BlackjackGame{
-    channel_game: ChannelGame,
-    game_token: Uuid,
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::{mpsc, Mutex};
+use uuid::Uuid;
+
+pub struct BlackjackGame {
+    pub game_token: Uuid,
     action_sender: Option<mpsc::Sender<GameAction>>,
     option_receiver: Option<mpsc::Receiver<Vec<GameAction>>>,
     thread_handle: Option<tokio::task::JoinHandle<()>>,
 }
 
-impl BlackjackGame{
-    pub async fn new() -> Self{
-        let mut game = BlackjackGame{
-            channel_game: ChannelGame::new(),
+impl BlackjackGame {
+    pub async fn new() -> Self {
+        let mut game = BlackjackGame {
             game_token: Uuid::new_v4(),
+            action_sender: None,
+            option_receiver: None,
+            thread_handle: None,
         };
         game.start().await;
         game
     }
 
-    pub async fn start(&mut self){
+    pub async fn start(&mut self) {
         let (action_sender, action_receiver) = mpsc::channel::<GameAction>(32);
-        let (option_sender, mut option_receiver) = mpsc::channel::<Vec<GameAction>>(32);
+        let (option_sender, option_receiver) = mpsc::channel::<Vec<GameAction>>(32);
         self.action_sender = Some(action_sender);
         self.option_receiver = Some(option_receiver);
         let option_sender_clone = option_sender.clone();
@@ -40,51 +44,80 @@ impl BlackjackGame{
     }
 }
 
-pub struct BlackjackService{
-    games: Mutex<HashMap<String, BlackjackGame>>,
+pub struct BlackjackService {
+    games: Mutex<HashMap<String, Arc<Mutex<BlackjackGame>>>>,
 }
 
-pub struct CreateGameResponse{
-    game_id: String,
-    game_token: Uuid,
+pub struct CreateGameResponse {
+    pub game_id: String,
+    pub game_token: Uuid,
 }
 
-impl CreateGameResponse{
-    pub fn new(game_id: String, game_token: Uuid) -> String{
-        CreateGameResponse{
+impl CreateGameResponse {
+    pub fn new(game_id: String, game_token: Uuid) -> CreateGameResponse {
+        CreateGameResponse {
             game_id,
             game_token,
         }
     }
 }
 
-impl BlackjackService{
-    pub fn new() -> Self{
-        BlackjackService{
+impl BlackjackService {
+    pub fn new() -> Self {
+        BlackjackService {
             games: Mutex::new(HashMap::new()),
         }
     }
 
-    pub async fn create_game(&mut self) -> String{
-        let game = BlackjackGame::new().await;
-        let guard = self.games.lock().await;
-        let mut data = guard.lock().await;
+    pub async fn create_game(&mut self) -> CreateGameResponse {
+        let game = Arc::new(Mutex::new(BlackjackGame::new().await));
+        let mut data = self.games.lock().await;
         let game_id = data.len().to_string();
-        data.insert(game_id.clone(), game);
-        CreateGameResponse::new(game_id, game.game_token)
+        data.insert(game_id.clone(), game.clone());
+        let token = game.lock().await.game_token;
+        CreateGameResponse::new(game_id, token)
     }
 
-    pub async fn delete_game(&mut self, game_id: String){
+    pub async fn get_game(&mut self, game_id: String) -> Option<Arc<Mutex<BlackjackGame>>> {
+        self.games.lock().await.get(&game_id).cloned()
+    }
+
+    pub async fn delete_game(&mut self, game_id: String) -> bool {
         let game = self.games.lock().await.remove(&game_id);
-        if let Some(game) = game{
-            game.action_sender.lock().await.send(GameAction::Quit).await;
-            game.thread_handle.lock().await.unwrap();
+        if let Some(game) = game {
+            let sender = &game.lock().await.action_sender;
+            if let Some(s) = sender {
+                s.send(GameAction::Stop).await;
+            } else {
+                return false;
+            }
+            if let Some(t) = &game.lock().await.thread_handle {
+                t.abort();
+            } else {
+                return false;
+            }
         }
+        true
     }
 
-    pub async fn play_game(&mut self, game_id: String, action: String){
-        if let Some(game) = self.games.lock().await.get_mut(&game_id){
-            game.channel_game.send(action);
+    pub async fn play_game(&mut self, game_id: String, action: String) {
+        if let Some(game) = self.games.lock().await.get_mut(&game_id) {
+            if let Some(sender) = &game.lock().await.action_sender {
+                sender
+                    .send(GameAction::from(
+                        action
+                            .to_lowercase()
+                            .as_str()
+                            .chars()
+                            .next()
+                            .unwrap()
+                            .to_ascii_lowercase(),
+                    ))
+                    .await;
+            }
+            if let Some(receiver) = game.lock().await.option_receiver.as_mut() {
+                let _options = receiver.recv().await;
+            }
         }
     }
 }
