40e364285877df2ee1a18841676a82b1d952e697
Michael Pohl <mpohl100@googlemail.com>
2024-07-03 16:16:47 +0000

    removed unnecessary lock around the blackjack service

--- src/server.rs
+++ src/server.rs
@@ -2,7 +2,6 @@ use blackjack_rust::service::domain::BlackjackService;
 use blackjack_rust::service::routes::{create_game, delete_game, play_game};
 
 use actix_web::{web, web::Data, App, HttpServer};
-use tokio::sync::Mutex;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -11,7 +10,7 @@ async fn main() -> std::io::Result<()> {
             .route("/blackjack", web::post().to(create_game))
             .route("/blackjack/{game_id}", web::delete().to(delete_game))
             .route("/blackjack/{game_id}/play", web::post().to(play_game))
-            .app_data(Data::new(Mutex::new(BlackjackService::new())))
+            .app_data(Data::new(BlackjackService::new()))
     })
     .bind("127.0.0.1:8080")?
     .run()
