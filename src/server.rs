5c60706c49505c00404de184e854317e23828d32
Michael Pohl <mpohl100@googlemail.com>
2025-09-13 21:26:17 +0000

    pass correct type to app data to get game creation to work

--- src/server.rs
+++ src/server.rs
@@ -2,12 +2,15 @@ use blackjack_rust::service::startup::run;
 use blackjack_rust::service::domain::BlackjackService;
 
 use std::net::TcpListener;
+use tokio::sync::Mutex;
 use std::sync::Arc;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
     let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind to port");
-    let blackjack_service = Arc::new(BlackjackService::new());
+    let blackjack_service = Arc::new(Mutex::new(BlackjackService::new()));
     run(listener, blackjack_service)?.await?;
     Ok(())
 }
